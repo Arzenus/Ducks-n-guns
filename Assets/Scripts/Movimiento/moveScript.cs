2be28ce637200e61e4451637f74534ae819c44bf
Neroride <nico_ser96@hotmail.com>
2020-08-26 15:32:41 -0300

    Movimiento terminado

--- Assets/Scripts/Movimiento/moveScript.cs
+++ Assets/Scripts/Movimiento/moveScript.cs
@@ -1,25 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class moveScript : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 0;
-    
-    Vector2 rotation = new Vector2 (0, 0);
-    public float speed = 3;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Camera nuevaCamera;
 
+    [SerializeField] private float sensibilidadX = 3;
+
+    [SerializeField] private float sensibilidadY = 5;
+
+    private float ejeX = 0, ejeY = 0;
+    
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, 0, 0);
+        ejeX += Input.GetAxis("Mouse X") * sensibilidadX;
+
+        ejeY += Input.GetAxis("Mouse Y") * sensibilidadY;
+
+        var rotacionX = Quaternion.AngleAxis(ejeX, Vector3.up);
+
+        var rotacionY = Quaternion.AngleAxis(ejeY, Vector3.left);
+
+        transform.localRotation = rotacionX * rotacionY;
     }
 }
