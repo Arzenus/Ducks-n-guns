2005259c9bac02170b993be3fa7d1ddfafd55d7b
Neroride <nico_ser96@hotmail.com>
2020-08-26 16:52:58 -0300

    proyectil agregado

---
+++ Assets/Scripts/Disparo/Disparo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public abstract class Disparo : MonoBehaviour
+{
+    [SerializeField] private float potencia = 0;
+
+    [SerializeField] private float danio = 0;
+
+    public void accion()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.Translate(potencia, 0, 0);
+        }
+    }
+}
